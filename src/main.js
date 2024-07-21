354a5f58734566e79cba28e434847865add8d9c0
Bob <noreply@github.com>
2024-07-21 21:04:32 +0800

    use scss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/main.scss'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
